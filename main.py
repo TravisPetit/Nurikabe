1767058f875dcab78ba590f9a73f27315300587a
TravisPetit <32329753+TravisPetit@users.noreply.github.com>
2023-10-22 22:19:05 +0200

    New feature that prints T and T prime graphs

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import argparse
 from parsing import parse_from_graph_str
-from nurikabe import create_nurikabe_instance, print_nurikabe_instance
+from nurikabe import create_nurikabe_instance, print_nurikabe_instance, create_t_graph, create_t_prime_graph
 
 
-def run(filename, draw_corresponding_vertices, draw_corresponding_edges, draw_blocked_cells):
+def run(filename, draw_corresponding_vertices, draw_corresponding_edges, draw_blocked_cells, draw_t_graph, draw_t_prime_grah):
     vertices, edges, s, t = parse_from_graph_str(filename)
     I, dim = create_nurikabe_instance(vertices, edges, s, t, draw_corresponding_vertices, draw_corresponding_edges,
-                                      draw_blocked_cells)
+            draw_blocked_cells)
+    t_graph, dim = create_t_graph(vertices, edges, s, t)
+    t_prime_graph = create_t_prime_graph(t_graph, s, t, dim)
+    print("Nurikabe instance:")
     print_nurikabe_instance(I, dim)
+    if draw_t_graph: 
+        print("T graph:")
+        print_nurikabe_instance(t_graph, dim)
+    if draw_t_prime_grah:
+        print("T prime graph:")
+        print_nurikabe_instance(t_prime_graph, dim)
 
 
 if __name__ == "__main__":
@@ -19,9 +28,15 @@ if __name__ == "__main__":
                         action=argparse.BooleanOptionalAction)
     parser.add_argument("-b", "--blocked", help="Draw blocked cells", default=False,
                         action=argparse.BooleanOptionalAction)
+    parser.add_argument("-T", "--tgraph", help="Draw T graph", default=False,
+                        action=argparse.BooleanOptionalAction)
+    parser.add_argument("-Tp", "--tprimegraph", help="Draw T prime graph", default=False,
+                        action=argparse.BooleanOptionalAction)
     args = parser.parse_args()
     fname = args.fname
     v = args.vertices
     e = args.edges
     b = args.blocked
-    run(fname, v, e, b)
+    tg = args.tgraph
+    tpg = args.tprimegraph
+    run(fname, v, e, b, tg, tpg)
--- nurikabe.py
+++ nurikabe.py
@@ -101,6 +101,92 @@ def insert_corresponding_edge(nurikabe_grid, e):
         nurikabe_grid[(xp + 3, yp)] = "-"
 
 
+def insert_tentacle_in_t_graph(t_graph, e):
+    x, y, x2, y2 = e
+    if x == x2:
+        if y == y2 + 1:
+            v = (x, y2)
+        else:
+            v = (x, y)
+        xp, yp = corresponding_vertex(v)
+        t_graph[(xp - 1, yp + 1)] = "o"
+        t_graph[(xp + 1, yp + 1)] = "o"
+        t_graph[(xp - 1, yp + 3)] = "o"
+        t_graph[(xp + 1, yp + 3)] = "o"
+    else:
+        if x == x2 + 1:
+            v = (x2, y)
+        else:
+            v = (x, y)
+        xp, yp = corresponding_vertex(v)
+        t_graph[(xp + 1, yp - 1)] = "o"
+        t_graph[(xp + 1, yp + 1)] = "o"
+        t_graph[(xp + 3, yp - 1)] = "o"
+        t_graph[(xp + 3, yp + 1)] = "o"
+
+
+def insert_edge_in_t_graph(t_graph, e):
+    x, y, x2, y2 = e
+    if x == x2:
+        if y == y2 + 1:
+            v = (x, y2)
+        else:
+            v = (x, y)
+        xp, yp = corresponding_vertex(v)
+        t_graph[(xp, yp + 1)] = "o"
+        t_graph[(xp, yp + 2)] = "o"
+        t_graph[(xp, yp + 3)] = "o"
+    else:
+        if x == x2 + 1:
+            v = (x2, y)
+        else:
+            v = (x, y)
+        xp, yp = corresponding_vertex(v)
+        t_graph[(xp + 1, yp)] = "o"
+        t_graph[(xp + 2, yp)] = "o"
+        t_graph[(xp + 3, yp)] = "o"
+
+
+def insert_vertex_in_t_graph(t_graph, v):
+    vp = corresponding_vertex(v)
+    t_graph[vp] = "o"
+
+
+def create_t_graph(grid_vertices, grid_edges, s, t):
+    xmax = max(grid_vertices, key=lambda x: x[0])[0]
+    ymax = max(grid_vertices, key=lambda x: x[1])[1]
+    dim = (max((xmax, ymax)) + 1) * 4 + 8
+    t_graph = {(x, y): " " for x in range(dim) for y in range(dim)}
+
+    for v in grid_vertices:
+        insert_vertex_in_t_graph(t_graph, v)
+
+    for e in grid_edges:
+        insert_edge_in_t_graph(t_graph, e)
+        insert_tentacle_in_t_graph(t_graph, e)
+
+    insert_s_label(t_graph, s)
+    insert_t_label(t_graph, t)
+    return t_graph, dim
+
+
+def create_t_prime_graph(t_graph, s,t, dim):
+    t_prime_graph = {(x, y): " " for x in range(dim) for y in range(dim)}
+    for x in range(1, dim-1):
+        for y in range(1, dim-1):
+            if t_graph[(x,y)] != " ":
+                degree = 0
+                for (dx, dy) in ((-1, 0), (1, 0), (0, -1), (0,1)):
+                    if t_graph[(x + dx, y + dy)] != " ":
+                        degree += 1
+                if degree > 1:
+                    t_prime_graph[(x, y)] = "o"
+
+    insert_s_label(t_prime_graph, s)
+    insert_t_label(t_prime_graph, t)
+    return t_prime_graph
+    
+
 def create_nurikabe_instance(grid_vertices, grid_edges, s, t,
                              draw_corresponding_vertices=False, draw_corresponding_edges=False,
                              draw_blocked_cells=False):
@@ -132,7 +218,6 @@ def create_nurikabe_instance(grid_vertices, grid_edges, s, t,
 
         if x == x2:
             insert_horizontal_bridge(nurikabe_grid, e)
-            pass
 
         else:
             insert_vertical_bridge(nurikabe_grid, e)
